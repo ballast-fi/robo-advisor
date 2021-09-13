61d66d5be8845166327675efd2f7ae22060592d6
panche <euBkIIOi$%^964>
2021-09-13 19:26:07 +0200

    Initial commit

---
+++ scripts/pool-create.sh
@@ -0,0 +1,4 @@
+truffle exec scripts/create-comp-strategy.js &&
+truffle exec scripts/create-aave-strategy.js &&
+truffle exec scripts/create-strategy-manager.js &&
+truffle exec scripts/create-pool.js
