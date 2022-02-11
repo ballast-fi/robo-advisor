0f25b27342efab8f2260335b66f778264103385c
Jace Grebski <jgrebski@gmail.com>
2022-02-10 21:14:55 -0500

    token name change to reflect whitepaper changes.
    updated token anme

--- contracts/BalastERC20.sol
+++ contracts/BalastERC20.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 ///	@title	Balast token contract
 contract BalastERC20 is ERC20 {
 
-	constructor(uint256 initialSupply) ERC20("Balast", "BLST") {
+	constructor(uint256 initialSupply) ERC20("Ballast Finance", "BALFI") {
 		_mint(msg.sender, initialSupply);
 	}
 
